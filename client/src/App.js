44745a9ed82cc4b202c6743cce4890635010a45e
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-03-27 20:31:24 -0600

    Steps one and two finished.  Functionality established for selecting rep vs sen and selecting state to build query.

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,93 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {reps: []}
+  state = {
+    states: ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC",  
+    "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",  
+    "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",  
+    "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",  
+    "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"],
 
-  componentDidMount() {
-    fetch('/representatives/CA')
-      .then(res => res.json())
-      .then(reps => this.setState({ reps: reps.results }));
+    reps: [],
+    selectedState: '',
+    isRepresentatives: 'representatives',
+
+  };
+
+  getRepresentatives = () => {
+    const endpoint = '/' + this.state.isRepresentatives + '/' + this.state.selectedState;
+    console.log(endpoint);
+
+    try {
+      fetch(endpoint)
+        .then(res => res.json())
+        .then(reps => this.setState({ reps: reps.results }));
+    } catch (e) {
+      console.log("Something went wrong- ", e);
+    }
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = /*target.type === 'checkbox' ? target.checked :*/ target.value;
+    const name = target.name;
+    console.log("value: " + value + " name: " + name);
+
+    this.setState({
+      [name]: value
+    }, () => {
+      if (this.state.selectedState)
+        this.getRepresentatives();
+    });
   }
 
   render() {
     return (
       <div className="App">
         <h1>Reps</h1>
+        <form>
+        <label>
+          Representative 
+          <input
+            name="isRepresentatives"
+            value="representatives"
+            type="checkbox"
+            checked={this.state.isRepresentatives === 'representatives'}
+            onChange={this.handleInputChange} />
+        </label>
+        <label>
+          Senator 
+          <input
+            name="isRepresentatives"
+            value="senators"
+            type="checkbox"
+            checked={this.state.isRepresentatives === 'senators'}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          State: 
+          <select name="selectedState" onChange={this.handleInputChange}>
+            <option selected value="" >-Select- </option>
+            {this.state.states.map((res, index) =>
+              <option key={index} value={res}>{res}</option>
+            )}
+          </select>
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
         {this.state.reps.map((rep, index) =>
           <div key={index}>{rep.name}</div>
         )}
+        {console.log("selectedState: " + this.state.selectedState)}
       </div>
     );
   }
